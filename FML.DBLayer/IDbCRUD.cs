5927fb5f93c60e6290b3e5c4c6e837674f27383c
Bodil <bodil.noergaard86@gmail.com>
2017-10-04 12:42:04 +0200

    Interface til DB

---
+++ FML.DBLayer/IDbCRUD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FML.DBLayer
+{
+    interface IDbCRUD<T>
+    {
+        //T er stand in for en hvilken som helst class, "entity" er pladsholder for navnet på instance af class T. 
+        void Create(T entity);
+        //T er stand in for hvilken som helst Class, int id er id for den specifikke instance af class "T" der ønskes fundet.
+        T Get(int Id);
+        //itererer over en collection af typen T ( måske giver den en liste af instance referencer?)
+        IEnumerable<T> GetAll();
+        // opdaterer instancen "entity" af class typen T
+        void Update(T entity);
+        //sletter en instance med id som er " int id"
+        void Delete(int id);
+
+    }
+}
