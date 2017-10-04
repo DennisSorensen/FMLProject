5dd52b2881efa4a0bfca5a1e531ac3247396359b
ann <ann.hvarregaard@gmail.com>
2017-10-04 12:39:08 +0200

    Tilføjede form for sale

--- FMLSale/Form1.cs
+++ FMLSale/Form1.cs
@@ -16,5 +16,30 @@ namespace FMLSale
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
