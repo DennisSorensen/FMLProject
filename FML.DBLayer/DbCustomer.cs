18f992b394873334bee48b51180d809703c04cf5
Bodil <bodil.noergaard86@gmail.com>
2017-10-04 13:31:17 +0200

    DB customer stuff

---
+++ FML.DBLayer/DbCustomer.cs
@@ -0,0 +1,76 @@
+﻿using FML.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Transactions;
+using System.Data.SqlClient;
+
+namespace FML.DBLayer
+{
+    class DbCustomer : IDbCRUD<Customer>
+    {
+        private readonly string CONNECTION_STRING = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public void Create(Customer customer)
+        {
+            TransactionOptions to = new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead };
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.RequiresNew, to))
+            {
+                using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+                {
+                    connection.Open();
+                    int amountOfBookings;
+                    using (SqlCommand cmd = connection.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT Count(*) FROM Booking WHERE StartTime <= @startTime AND EndTime>=@endTime AND RoomId=@roomId";
+                        cmd.Parameters.AddWithValue("roomId", entity.RoomId);
+                        cmd.Parameters.AddWithValue("startTime", entity.StartTime);
+                        cmd.Parameters.AddWithValue("endTime", entity.EndTime);
+                        amountOfBookings = (int)cmd.ExecuteScalar();
+                    }
+                    if (amountOfBookings == 0)
+                    {
+                        using (SqlCommand cmd = connection.CreateCommand())
+                        {
+                            cmd.CommandText = "INSERT INTO Booking (RoomId, UserId, StartTime,EndTime) VALUES(@roomId, @userId,@startTime,@endTime)";
+                            cmd.Parameters.AddWithValue("roomId", entity.RoomId);
+                            cmd.Parameters.AddWithValue("userId", entity.UserId);
+                            cmd.Parameters.AddWithValue("startTime", entity.StartTime);
+                            cmd.Parameters.AddWithValue("endTime", entity.EndTime);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    else
+                    {
+                        throw new FaultException<Exception>(new Exception("Der er allerede en booking"));
+                    }
+                }
+                scope.Complete();
+            }
+        }
+    }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- FML.Models/Customer.cs
+++ FML.Models/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FML.Models
 {
-    class Customer
+    public class Customer
     {
         public int CustomerId { get; set; }
         public bool Commercial { get; set; }
