7b2a8be6c4e37354e98a06169496b1a0a30e6854
Dennis Sørensen <dennissorensen24@hotmail.dk>
2017-10-04 12:05:16 +0200

    Added alt i model lag

---
+++ FML.Models/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FML.Models
+{
+    class Sale
+    {
+        public int SaleId { get; set; }
+        public DateTime Date { get; set; }
+        public Customer Customer { get; set; }
+        List<SalesLineItem> salesLineItem;
+    }
+}
