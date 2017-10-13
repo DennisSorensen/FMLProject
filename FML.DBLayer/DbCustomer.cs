704638cb9879bb2bcef71ea033b2c03796b82503
Bodil <bodil.noergaard86@gmail.com>
2017-10-13 10:10:54 +0200

    DBCustomer færdig

--- FML.DBLayer/DbCustomer.cs
+++ FML.DBLayer/DbCustomer.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Transactions;
 using System.Data.SqlClient;
+using System.ServiceModel;
 
 namespace FML.DBLayer
 {
@@ -22,55 +23,130 @@ namespace FML.DBLayer
                 using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
                 {
                     connection.Open();
-                    int amountOfBookings;
-                    using (SqlCommand cmd = connection.CreateCommand())
-                    {
-                        cmd.CommandText = "SELECT Count(*) FROM Booking WHERE StartTime <= @startTime AND EndTime>=@endTime AND RoomId=@roomId";
-                        cmd.Parameters.AddWithValue("roomId", entity.RoomId);
-                        cmd.Parameters.AddWithValue("startTime", entity.StartTime);
-                        cmd.Parameters.AddWithValue("endTime", entity.EndTime);
-                        amountOfBookings = (int)cmd.ExecuteScalar();
-                    }
-                    if (amountOfBookings == 0)
+
+
+                    try
                     {
                         using (SqlCommand cmd = connection.CreateCommand())
                         {
-                            cmd.CommandText = "INSERT INTO Booking (RoomId, UserId, StartTime,EndTime) VALUES(@roomId, @userId,@startTime,@endTime)";
-                            cmd.Parameters.AddWithValue("roomId", entity.RoomId);
-                            cmd.Parameters.AddWithValue("userId", entity.UserId);
-                            cmd.Parameters.AddWithValue("startTime", entity.StartTime);
-                            cmd.Parameters.AddWithValue("endTime", entity.EndTime);
+                            cmd.CommandText = "INSERT INTO Customer (CustomerId, Commercial, Name, Address, Email) VALUES(@CustomerId, @Commercial, @Name, @Address, @Email)";
+                            cmd.Parameters.AddWithValue("CustomerId", customer.CustomerId);
+                            cmd.Parameters.AddWithValue("Commercial", customer.Commercial);
+                            cmd.Parameters.AddWithValue("Name", customer.Name);
+                            cmd.Parameters.AddWithValue("Adress", customer.Address);
+                            cmd.Parameters.AddWithValue("Email", customer.Email);
                             cmd.ExecuteNonQuery();
                         }
                     }
-                    else
+                    catch (SqlException e)
                     {
-                        throw new FaultException<Exception>(new Exception("Der er allerede en booking"));
+                        throw e;
                     }
                 }
                 scope.Complete();
             }
         }
-    }
+    
 
         public void Delete(int id)
+         { 
+        using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
         {
-            throw new NotImplementedException();
-        }
+            connection.Open();
 
-        public Customer Get(int Id)
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "DELETE FROM Customer WHERE Id=@id";
+                cmd.Parameters.AddWithValue("id", id);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+
+        public Customer Get(int CustomerId)
         {
-            throw new NotImplementedException();
+            Customer customer = null;
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Open();
+
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Customer WHERE Id=@CustomerId";
+                    cmd.Parameters.AddWithValue("CustomerId", CustomerId);
+                    var reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        customer = new Customer
+                        {
+                            
+                            CustomerId = (int)reader["CustomerId"],
+                            Commercial = (bool)reader["Commercial"],
+                            Name = (String)reader["Name"],
+                            Address = (String)reader["Adress"],
+                            Email = (String)reader["Email"]
+
+                        };
+                    }
+                }
+
+            }
+            return customer; 
         }
 
         public IEnumerable<Customer> GetAll()
         {
-            throw new NotImplementedException();
+            List<Customer> customers = new List<Customer>();
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Open();
+
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Customer";
+                    var reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Customer c = new Customer
+                        {
+                            CustomerId = (int)reader["CustomerId"],
+                            Commercial = (bool)reader["Commercial"],
+                            Name = (String)reader["Name"],
+                            Address = (String)reader["Adress"],
+                            Email = (String)reader["Email"]
+                         
+                        };
+                        customers.Add(c);
+                    }
+                }
+
+            }
+            return customers;
         }
 
-        public void Update(Customer entity)
+        public void Update(Customer customer)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Open();
+
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "UPDATE Customer SET CustomerID=@cusId, Commercial=@comId, Name=@name, Address=@address, Email=@email WHERE ID=@CustomerId";
+                    cmd.Parameters.AddWithValue("cusId", customer.CustomerId);
+                    cmd.Parameters.AddWithValue("comId", customer.Commercial);
+                    cmd.Parameters.AddWithValue("name", customer.Name);
+                    cmd.Parameters.AddWithValue("address", customer.Address);
+                    cmd.Parameters.AddWithValue("email", customer.Email);
+                    cmd.Parameters.AddWithValue("CustomerId", customer.CustomerId);
+                    cmd.ExecuteNonQuery();
+
+                }
+
+            }
         }
     }
 }
